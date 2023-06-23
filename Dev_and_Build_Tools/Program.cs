112262fb4f09360c9bc4690ba59227d657a062cc
Valentina Strazhnikava <strvvalentina@gmail.com>
2023-06-23 15:35:35 +0300

    Add Development and Build Tools

---
+++ Dev_and_Build_Tools/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Dev_and_Build_Tools
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please write a text");
+
+			string text = Console.ReadLine();
+
+			Console.WriteLine($"Lenght unequal consecutive characters is {CheckText.GetLenghtUnequalConsecutiveCharacters(text)}.");
+		}
+	}
+}
