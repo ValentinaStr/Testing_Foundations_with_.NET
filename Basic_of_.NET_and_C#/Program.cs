086a49043f5d827c68d3a1cf41905896357c0747
Valentina Strazhnikava <strvvalentina@gmail.com>
2023-06-28 01:49:49 +0300

    Add TestsBasic of .NET and C# with tests

---
+++ Basic_of_.NET_and_C#/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicOf.NET
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = NumberConversion.CheckNumberToConvert();
+			int notation = NumberConversion.CheckNotationToConvert();
+			Console.WriteLine($"{number} in the number system the base {notation} is {NumberConversion.Conversation(number, notation)}.");
+		}
+	}
+}
