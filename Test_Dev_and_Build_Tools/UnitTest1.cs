4423ffff45379441bd2c57946f8e8d8913caa23b
Valentina Strazhnikava <strvvalentina@gmail.com>
2023-08-02 12:49:14 +0300

    Code refactored

--- Test_Dev_and_Build_Tools/UnitTest1.cs
+++ Test_Dev_and_Build_Tools/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Dev_and_Build_Tools;
+
 namespace Test_Dev_and_Build_Tools
 {
 	[TestClass]
@@ -10,30 +11,35 @@ namespace Test_Dev_and_Build_Tools
 			string text = "";
 			Assert.AreEqual(0, CheckText.GetLenghtUnequalConsecutiveCharacters(text));
 		}
+
 		[TestMethod]
 		public void CheckStirngWithOneLetter()
 		{
 			string text = "a";
 			Assert.AreEqual(1, CheckText.GetLenghtUnequalConsecutiveCharacters(text));
 		}
+
 		[TestMethod]
 		public void CheckStirngLongtAtTheBeginning()
 		{
 			string text = "asdffg";
 			Assert.AreEqual(4, CheckText.GetLenghtUnequalConsecutiveCharacters(text));
 		}
+
 		[TestMethod]
 		public void CheckStirngrlongAtTheEnd()
 		{
 			string text = "asdffgwsedr";
 			Assert.AreEqual(7, CheckText.GetLenghtUnequalConsecutiveCharacters(text));
 		}
+
 		[TestMethod]
 		public void CheckStirngWithDifferentCharacters()
 		{
 			string text = "1234567890";
 			Assert.AreEqual(text.Length, CheckText.GetLenghtUnequalConsecutiveCharacters(text));
 		}
+
 		[TestMethod]
 		public void CheckStirngWithTheEqualCharacters()
 		{
